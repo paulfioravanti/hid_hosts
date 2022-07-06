0b1f0098b0d584beec33db593300090289fe9724
Paul Fioravanti <paul.fioravanti@gmail.com>
2022-07-06 14:15:53 +1000

    Make shell script take in a parameter, rather than use a hard-coded value

--- hid_host.sh
+++ hid_host.sh
@@ -2,8 +2,8 @@
 # This script was created for use with an Elgato Stream Deck Pedal
 # Assign a System > Open action to a pedal and reference this script
 # in the "App/File" field.
-# In this case, the argument `1` will trigger a Gaming layer toggle in my Georgi
-# firmware. See `raw_hid_receive` definition in:
+# See `raw_hid_receive` definition in my Georgi keymap for how the parameter
+# gets handled:
 # https://github.com/paulfioravanti/qmk_keymaps/blob/master/keyboards/gboards/georgi/keymaps/paulfioravanti/keymap.c
 
-./hid_host 1
+./hid_host "$1"
