e084810986c242363b493a7ead3ae4c823bb3d6d
Paul Fioravanti <paul.fioravanti@gmail.com>
2022-07-06 10:43:07 +1000

    Add debugging and retry code for hid_open

--- hid_host.c
+++ hid_host.c
@@ -1,7 +1,9 @@
+#include <stdio.h> // printf
 #include <stdlib.h> // strtol
 #include <string.h> // strlen
 #include <errno.h> // errno
 #include <limits.h> // INT_MIN, INT_MAX
+#include <unistd.h> // usleep
 #include <hidapi.h>
 
 // VID and PID for Georgi
@@ -10,8 +12,15 @@
 #define PRODUCT_ID 0x1337
 
 int main(int argc, char* argv[]) {
+  // Requires only one argument
+  if (argc != 2) {
+    printf("Only one argument allowed\n");
+    return 1;
+  }
+
   // REF: https://stackoverflow.com/questions/9748393/how-can-i-get-argv-as-int
   if (strlen(argv[1]) == 0) {
+    printf("Argument cannot be an empty string\n");
     return 1; // empty string
   }
 
@@ -23,32 +32,57 @@ int main(int argc, char* argv[]) {
   // - an invalid character was found before the end of the string
   // - overflow or underflow errors occurred
   if (*string_end != '\0' || errno != 0) {
+      printf("Invalid character %s\n", argv[1]);
       return 1;
   }
   // Check that the number is within the limited capacity of an int
   if (arg < INT_MIN || arg > INT_MAX) {
+      printf("Integer value is out of bounds");
       return 1;
   }
   int arg_int = arg;
 
   int res;
-  unsigned char buf[65];
+  unsigned char buf[1];
   hid_device *device;
 
   // Initialize the hidapi library
   res = hid_init();
+  if (res < 0) {
+    printf("Unable to initialize hidapi library\n");
+    return 1;
+  }
 
+  int max_retries = 20;
+  int current_retry = 1;
   // Open the device using the VID, PID,
   device = hid_open(VENDOR_ID, PRODUCT_ID, NULL);
 
+  while (!device) {
+    usleep(50000);
+    device = hid_open(VENDOR_ID, PRODUCT_ID, NULL);
+    if (current_retry > max_retries) {
+      printf("Unable to open device after %d retries\n", max_retries);
+      return 1;
+    }
+    printf("Open device retry: %d\n", current_retry);
+    current_retry++;
+  }
+  printf("Device opened successfully\n");
+
   buf[0] = arg_int;
-  res = hid_write(device, buf, 65);
+  res = hid_write(device, buf, 1);
+
+  if (res < 0) {
+    printf("Unable to write()\n");
+    printf("Error: %ls\n", hid_error(device));
+  }
 
   // Close the device
   hid_close(device);
 
   // Finalize the hidapi library
-  res = hid_exit();
+  hid_exit();
 
   return 0;
 }
