d300f90b5623b6b13a148cd80d0965df34665c51
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-08-14 16:58:06 +1000

    Refactor public interface in the header file

--- hid_host.c
+++ hid_host.c
@@ -1,4 +1,47 @@
+#include <errno.h>         // errno
+#include <hidapi.h>        // hid_close, hid_device, hid_device_info, hid_error,
+                           // hid_exit, hid_init, hid_read, hid_write,
+                           // open_device
+#include <hidapi_darwin.h> // hid_darwin_set_open_exclusive
+#include <limits.h>        // INT_MAX, INT_MIN
+#include <stdio.h>         // printf, snprintf
+#include <stdlib.h>        // strtol
+#include <string.h>        // memset, strlen
+#include <unistd.h>        // usleep
+#include <steno_tape.h>    // MAX_MESSAGE_LENGTH, steno_tape_cleanup,
+                           // steno_tape_error, steno_tape_gaming_mode,
+                           // steno_tape_init, steno_tape_mode_unchanged,
+                           // steno_tape_steno_mode, Tape
 #include "hid_host.h"
+
+// VID and PID for Georgi
+// REF: https://github.com/qmk/qmk_firmware/blob/master/keyboards/gboards/georgi/config.h
+#define VENDOR_ID  0xFEED
+#define PRODUCT_ID 0x1337
+
+enum {
+  BUFFER_LENGTH = 2,
+  ENABLE_NONBLOCKING = 1,
+  GAMING_MODE = 0x3,
+  HID_DARWIN_NON_EXCLUSIVE_MODE = 0,
+  HID_OPEN_MAX_RETRIES = 30,
+  HID_OPEN_SLEEP_MICROSECONDS = 15000, // 15 ms
+  HID_READ_MAX_RETRIES = 3,
+  HID_READ_SLEEP_MICROSECONDS = 500000, // 500 ms
+  MODE_UNCHANGED = 0x9,
+  STENO_MODE = 0x4
+};
+
+static const char HID_INIT_FAIL_MESSAGE[] =
+  " Unable to initialize HID library!\n";
+static const char DEVICE_OPEN_FAIL_MESSAGE[] =
+  " Couldn't OPEN device!\n";
+static const char DEVICE_WRITE_FAIL_MESSAGE[] =
+  " Couldn't WRITE to device!\n";
+static const char DEVICE_READ_FAIL_MESSAGE[] =
+  " Couldn't READ from device!\n";
+static const char HID_READ_BAD_VALUE_MESSAGE[] =
+  " Unexpected response from device: ";
 
 int main(int argc, char *argv[]) {
   int arg = parse_arguments(argc, argv);
--- hid_host.h
+++ hid_host.h
@@ -1,45 +1,9 @@
-#include <errno.h>         // errno
-#include <hidapi.h>        // hid_close, hid_device, hid_error, hid_exit,
-                           // hid_init, hid_read, hid_write, open_device
-#include <hidapi_darwin.h> // hid_darwin_set_open_exclusive
-#include <limits.h>        // INT_MAX, INT_MIN
-#include <stdio.h>         // printf, snprintf
-#include <stdlib.h>        // strtol
-#include <string.h>        // memset, strlen
-#include <unistd.h>        // usleep
-#include <steno_tape.h>    // steno_tape_cleanup, steno_tape_error,
-                           // steno_tape_gaming_mode, steno_tape_init,
-                           // steno_tape_mode_unchanged, steno_tape_steno_mode
-                           // Tape
+#pragma once
+#ifndef HID_HOST_H
+#define HID_HOST_H
 
-// VID and PID for Georgi
-// REF: https://github.com/qmk/qmk_firmware/blob/master/keyboards/gboards/georgi/config.h
-#define VENDOR_ID  0xFEED
-#define PRODUCT_ID 0x1337
-
-enum {
-  BUFFER_LENGTH = 2,
-  ENABLE_NONBLOCKING = 1,
-  GAMING_MODE = 0x3,
-  HID_DARWIN_NON_EXCLUSIVE_MODE = 0,
-  HID_OPEN_MAX_RETRIES = 30,
-  HID_OPEN_SLEEP_MICROSECONDS = 15000, // 15 ms
-  HID_READ_MAX_RETRIES = 3,
-  HID_READ_SLEEP_MICROSECONDS = 500000, // 500 ms
-  MODE_UNCHANGED = 0x9,
-  STENO_MODE = 0x4
-};
-
-static const char HID_INIT_FAIL_MESSAGE[] =
-  " Unable to initialize HID library!\n";
-static const char DEVICE_OPEN_FAIL_MESSAGE[] =
-  " Couldn't OPEN device!\n";
-static const char DEVICE_WRITE_FAIL_MESSAGE[] =
-  " Couldn't WRITE to device!\n";
-static const char DEVICE_READ_FAIL_MESSAGE[] =
-  " Couldn't READ from device!\n";
-static const char HID_READ_BAD_VALUE_MESSAGE[] =
-  " Unexpected response from device: ";
+#include <hidapi.h>     // hid_device, hid_device_info
+#include <steno_tape.h> // Tape
 
 int parse_arguments(int argc, char *argv[]);
 hid_device* get_or_open_device(void);
@@ -49,3 +13,5 @@ void read_device_message(hid_device *handle, unsigned char *buf, Tape *tape);
 void log_out_read_message(int message, Tape *tape);
 void clean_up(Tape *tape);
 void print_buffer(unsigned char *buf);
+
+#endif
